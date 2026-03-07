7ba5c2bcf233b3842db40d903ba144d90318317c
pablosantamaria26 <santamariapablodaniel@gmail.com>
2026-03-07 11:33:13 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Flota ML — Service Worker v25
-const CACHE = 'flota-ml-v25';
+// Flota ML — Service Worker v26
+const CACHE = 'flota-ml-v26';
 const STATIC = ['/LogisticaML/', '/LogisticaML/index.html'];
 
 self.addEventListener('install', e => {
